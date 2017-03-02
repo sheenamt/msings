f0a33a3f66242807dc3c8a74099ea9be2a3af9ac
sheenatodhunter <sheenams@uw.edu>
2017-03-02 09:57:53 -0800

    run_msings works on a list of bams

--- run_msings.sh
+++ run_msings.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
 
-BAM=$1;
+INTERVALS_FILE=$1;
+BEDFILE=$2;
+MSI_BASELINE=$3;
+REF_GENOME=$4;
+BAM_LIST=${5:}
 
-SAVEPATH=$2
-PFX=$3
-INTERVALS_FILE=$4;
-BEDFILE=$5;
-MSI_BASELINE=$6;
-REF_GENOME=$7;
+#"multiplier" is the number of standard deviations from the baseline that is required to call instability
+multiplier=2.0 
+#"msi_min_threshold" is the maximum fraction of unstable sites allowed to call a specimen MSI negative     
+msi_min_threshold=0.2
+#"msi_max_threshold" is the minimum fraction of unstable sites allowed to call a specimen MSI positive
+msi_max_threshold=0.2
 
-mkdir -p $SAVEPATH
+SAVEPATH=$(dirname $BAM)
 
-echo “Starting Analysis of $PFX” >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
+for BAM in $BAM_LIST; do
+    BAMNAME=$(basename $BAM)
+    PFX=${BAMNAME%.}
+    
+    
+    mkdir -p $SAVEPATH/$PFX
 
-echo "Making mpileups" >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-samtools mpileup -f $REF_GENOME -d 100000 -A -E  $BAM -l $INTERVALS_FILE | awk '{if($4 != 0) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
+    echo “Starting Analysis of $PFX” >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
 
-echo "Varscan Readcounts start" >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-java -Xmx4g -jar $VARSCAN readcounts $SAVEPATH/$PFX/$PFX.mpileup --variants-file $INTERVALS_FILE --min-base-qual 10 --output-file $SAVEPATH/$PFX/$PFX.msi_output &
-wait
+    echo "Making mpileups" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    samtools mpileup -f $REF_GENOME -d 100000 -A -E  $BAM -l $INTERVALS_FILE | awk '{if($4 != 0) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
+    
+    echo "Varscan Readcounts start" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    java -Xmx4g -jar $VARSCAN readcounts $SAVEPATH/$PFX/$PFX.mpileup --variants-file $INTERVALS_FILE --min-base-qual 10 --output-file $SAVEPATH/$PFX/$PFX.msi_output &
+    wait
 
-echo "MSI Analyzer start" >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
+    echo "MSI Analyzer start" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFXmsi_run_log.txt;
+    
+    msi analyzer $SAVEPATH/$PFX/$PFX.msi_output $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
+    
+    echo "MSI calls start" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    
+    msi count_msi_samples $MSI_BASELINE $SAVEPATH/$PFX --multiplier=$multiplier msi_min_threshold=$msi_min_threshold msi_max_threshold=$msi_max_threshold -o $SAVEPATH/$PFX/$PFX.MSI_Analysis.txt
 
-msi analyzer $SAVEPATH/$PFX/$PFX.msi_output $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
+    echo “Completed Analysis of $PFX” >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
 
-echo "MSI calls start" >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
+fi
 
-msi count_msi_samples $MSI_BASELINE $SAVEPATH/$PFX -o $SAVEPATH/$PFX/$PFX.MSI_Analysis.txt
-
-echo “Completed Analysis of $PFX” >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-
-exit
-
-#NOTE:
-#command to make "top-level" output, run after all individual samples are done.
-#msi count_msi_samples $MSI_BASELINE $SAVEPATH -o $SAVEPATH/Combined_MSI.txt
+echo "Creating summary analysis file for all samples" >> $SAVEPATH/msi_run_log.txt;
+msi count_msi_samples $MSI_BASELINE $SAVEPATH -o $SAVEPATH/Combined_MSI.txt
 
 
 
