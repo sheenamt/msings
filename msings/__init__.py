68d1b49bc2172c4db183f8bb34b7a609949854f8
sheenamt <sheena.todhunter@gmail.com>
2020-07-02 15:55:32 -0700

    fix error handling in __init__ for py3

--- msings/__init__.py
+++ msings/__init__.py
@@ -6,7 +6,7 @@ try:
     with open(join(_data, 'sha')) as s, open(join(_data, 'ver')) as v:
         sha = s.read().strip()
         ver = int(v.read())
-except Exception, e:
+except Exception as e:
     __version__ = ''
 else:
     __version__ = '%04i.%s' % (ver, sha)
