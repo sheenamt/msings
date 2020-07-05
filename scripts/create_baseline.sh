96ec90c409404e40d96a0abdf9fc778792957479
sheenamt <sheena.todhunter@gmail.com>
2020-07-04 20:53:01 -0700

    no longer use varscan, remove std from baseline by default

--- scripts/create_baseline.sh
+++ scripts/create_baseline.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-source msings-env/bin/activate
-VARSCAN=msings-env/bin/VarScan.v2.3.7.jar
+set -e
+source /mnt/disk10/users/sheenams/msings-v3.5/msings-v3.6-py3/bin/activate
+
 #BAM_LIST is a file of absolute paths to each bam file
-BAM_LIST=$1
-INTERVALS_FILE=$2;
-BEDFILE=$3;
+BAM_LIST=$1;
+BEDFILE=$2;
+MSI_BASELINE=$3;
 REF_GENOME=$4;
+
+#Check for required variables:
+if [ -z "$BAM_LIST" ]; then echo "BAM_LIST is unset" && exit ; else echo "BAM_LIST is set to '$BAM_LIST'"; fi
+if [ -z "$BEDFILE" ]; then echo "BEDFILE is unset" && exit ; else echo "BEDFILE is set to '$BEDFILE'"; fi
+if [ -z "$MSI_BASELINE" ]; then echo "MSI_BASELINE is unset" && exit ; else echo "MSI_BASELINE is set to '$MSI_BASELINE'"; fi
+if [ -z "$REF_GENOME"]; then echo "REF_GENOME is unset" && exit ; else echo "REF_GENOME is set to '$REF_GENOME'"; fi
 
 for BAM in `sed '/^$/d' $BAM_LIST`; do
     SAVEPATH=$(dirname $BAM)
@@ -15,33 +22,39 @@ for BAM in `sed '/^$/d' $BAM_LIST`; do
 
     mkdir -p $SAVEPATH/$PFX
 
-    echo “Starting Analysis of $PFX” >> $SAVEPATH/msi_run_log.txt;
-    date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
+    echo “Starting Analysis of $PFX” >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
 
-    echo "Making mpileups" >> $SAVEPATH/msi_run_log.txt;
-    date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-    samtools mpileup -f $REF_GENOME -d 100000 -A -E  $BAM -l $INTERVALS_FILE | awk '{if($4 >= 6) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
+    echo "sorting bam of $PFX" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    samtools sort $BAM $SAVEPATH/$PFX/$PFX.sorted && samtools index $SAVEPATH/$PFX/$PFX.sorted.bam
+
+    echo "Making mpileups" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    samtools mpileup -f $REF_GENOME -d 100000 -A -E -l $BEDFILE $SAVEPATH/$PFX/$PFX.sorted.bam| awk '{if($4 >= 6) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
     
-    echo "Varscan Readcounts start" >> $SAVEPATH/msi_run_log.txt;
-    date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-    java -Xmx4g -jar $VARSCAN readcounts $SAVEPATH/$PFX/$PFX.mpileup --variants-file $INTERVALS_FILE --min-base-qual 10 --output-file $SAVEPATH/$PFX/$PFX.msi_output &
-    wait
+    echo "MSI Analyzer start" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    
+    msi analyzer $SAVEPATH/$PFX/$PFX.mpileup $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
+    
+    echo "MSI calls start" >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
+     
+    msi count_msi_samples $MSI_BASELINE $SAVEPATH/$PFX -m $multiplier -t $msi_min_threshold $msi_max_threshold -o $SAVEPATH/$PFX/$PFX.MSI_Analysis.txt
 
-    echo "MSI Analyzer start" >> $SAVEPATH/msi_run_log.txt;
-    date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
-
-    msi analyzer $SAVEPATH/$PFX/$PFX.msi_output $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
+    echo “Completed Analysis of $PFX” >> $SAVEPATH/$PFX/msi_run_log.txt;
+    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
 
 done
 
 echo "Creating baseline of all files" >> $SAVEPATH/msi_run_log.txt;
 date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
 
-msi create_baseline $SAVEPATH -o $SAVEPATH/MSI_BASELINE.txt
+msi create_baseline $SAVEPATH -o $SAVEPATH/MSI_BASELINE.txt_temp
 
-echo “Completed Analysis of $PFX” >> $SAVEPATH/msi_run_log.txt;
-date +"%D %H:%M" >> $SAVEPATH/msi_run_log.txt;
+#Now, remove all positions with a STD of 0.0
+grep -Pv '\t0.0\t' $SAVEPATH/MSI_BASELINE.txt_temp > $SAVEPATH/MSI_BASELINE.txt
 
-exit
 
 
