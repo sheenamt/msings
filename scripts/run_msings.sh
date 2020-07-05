cc67701c6bf8d0cc9b0c29a62936da37025f3db7
sheenamt <sheena.todhunter@gmail.com>
2020-07-04 20:53:22 -0700

    remove varscan, check for input variables in wrapper script

--- scripts/run_msings.sh
+++ scripts/run_msings.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
-source msings-env/bin/activate
-VARSCAN=msings-env/bin/VarScan.v2.3.7.jar
+set -e
+source /mnt/disk10/users/sheenams/msings-v3.5/msings-v3.6-py3/bin/activate
 
 #BAM_LIST is a file of absolute paths to each bam file
-BAM_LIST=$1
-INTERVALS_FILE=$2;
-BEDFILE=$3;
+BAM_LIST=$1;
+BEDFILE=$2;
+MSI_BASELINE=$3;
 REF_GENOME=$4;
-MSI_BASELINE=$5;
+
+#Check for required variables:
+if [ -z "$BAM_LIST" ]; then echo "BAM_LIST is unset" && exit ; else echo "BAM_LIST is set to '$BAM_LIST'"; fi
+if [ -z "$BEDFILE" ]; then echo "BEDFILE is unset" && exit ; else echo "BEDFILE is set to '$BEDFILE'"; fi
+if [ -z "$MSI_BASELINE" ]; then echo "MSI_BASELINE is unset" && exit ; else echo "MSI_BASELINE is set to '$MSI_BASELINE'"; fi
+if [ -z "$REF_GENOME"]; then echo "REF_GENOME is unset" && exit ; else echo "REF_GENOME is set to '$REF_GENOME'"; fi
+
 
 #"multiplier" is the number of standard deviations from the baseline that is required to call instability
 multiplier=2.0 
@@ -16,7 +21,6 @@ multiplier=2.0
 msi_min_threshold=0.2
 #"msi_max_threshold" is the minimum fraction of unstable sites allowed to call a specimen MSI positive
 msi_max_threshold=0.2
-
 
 for BAM in `sed '/^$/d' $BAM_LIST`; do
     SAVEPATH=$(dirname $BAM)
@@ -34,17 +38,12 @@ for BAM in `sed '/^$/d' $BAM_LIST`; do
 
     echo "Making mpileups" >> $SAVEPATH/$PFX/msi_run_log.txt;
     date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
-    samtools mpileup -f $REF_GENOME -d 100000 -A -E  $SAVEPATH/$PFX/$PFX.sorted.bam -l $INTERVALS_FILE | awk '{if($4 >= 6) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
+    samtools mpileup -f $REF_GENOME -d 100000 -A -E  -l $BEDFILE $SAVEPATH/$PFX/$PFX.sorted.bam | awk '{if($4 >= 6) print $0}' > $SAVEPATH/$PFX/$PFX.mpileup 
     
-    echo "Varscan Readcounts start" >> $SAVEPATH/$PFX/msi_run_log.txt;
-    date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
-    java -Xmx4g -jar $VARSCAN readcounts $SAVEPATH/$PFX/$PFX.mpileup --variants-file $INTERVALS_FILE --min-base-qual 10 --output-file $SAVEPATH/$PFX/$PFX.msi_output &
-    wait
-
     echo "MSI Analyzer start" >> $SAVEPATH/$PFX/msi_run_log.txt;
     date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
     
-    msi analyzer $SAVEPATH/$PFX/$PFX.msi_output $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
+    msi analyzer $SAVEPATH/$PFX/$PFX.mpileup $BEDFILE -o $SAVEPATH/$PFX/$PFX.msi.txt
     
     echo "MSI calls start" >> $SAVEPATH/$PFX/msi_run_log.txt;
     date +"%D %H:%M" >> $SAVEPATH/$PFX/msi_run_log.txt;
@@ -58,6 +57,6 @@ done
 
 echo "Creating summary analysis file for all samples" >> $SAVEPATH/msi_run_log.txt;
 msi count_msi_samples $MSI_BASELINE $SAVEPATH -m $multiplier -t $msi_min_threshold $msi_max_threshold -o $SAVEPATH/Combined_MSI.txt
-
+ 
 
 
